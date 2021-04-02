c46ceac57d79d266efb46c102cc0ff5750531983
space-mvc <mrdaniellee2020@gmail.com>
2021-04-02 13:42:14 +0100

    update

--- lib/Model.php
+++ lib/Model.php
@@ -682,7 +682,7 @@ class Model
 	 */
 	private function is_delegated($name, &$delegate)
 	{
-		if ($delegate['prefix'] != '')
+		if (is_array($delegate) && $delegate['prefix'] != '')
 			$name = substr($name,strlen($delegate['prefix'])+1);
 
 		if (is_array($delegate) && in_array($name,$delegate['delegate']))
